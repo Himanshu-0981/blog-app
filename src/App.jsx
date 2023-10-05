148199f0b9f06989f77248f75ce5e895261cfbaa
Himanshu <kumarhimanshu2219@gmail.com>
2023-10-05 22:52:54 +0530

    login logout status fn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+
+import authService from "./services/auth_service";
+import { login, logout } from "./features/authSlice";
+import { Header, Footer } from "./components";
+
 function App() {
-  return (
+  const [loading, setLoading] = React.useState(true);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    statusLoginLogout();
+  });
+
+  const statusLoginLogout = async () => {
+    try {
+      const userData = await authService.getCurrentUser();
+      if (userData) dispatch(login({ userData }));
+      dispatch(logout());
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return !loading ? (
     <>
-      <h1>Hello react</h1>
+      <Header />
+      <Footer />
     </>
-  );
+  ) : null;
 }
 
 export default App;
